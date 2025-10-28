a1047c957cea707727f90a6c5f7d92be5c00e6bc
miklvv <mikael2735@gmail.com>
2025-10-28 14:07:30 +0100

    feat: couche interfaces avec objet Client + lombok

---
+++ src/main/java/com/example/demo/interfaces/clients/model/input/ClientInput.java
@@ -0,0 +1,30 @@
+package com.example.demo.interfaces.clients.model.input;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serial;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ClientInput extends AbstractInput {
+    @Serial
+    private static final long serialVersionUID = -6256916847598118362L;
+    private String lastname;
+    private String firstname;
+    private String genre;
+
+    public static Client convert(final ClientInput input) {
+        return Client.builder()
+                .identifier(UUID.randomUUID())
+                .lastname(input.getLastname())
+                .firstname(input.getFirstname())
+                .genre(GenreEnum.fromOrDefault(input.getGenre()))
+                .build();
+    }
+}
